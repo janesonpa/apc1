131b10a697592c9d24fe95d69e38efba5dc32a94
janesonpa <pjaneson@gmail.com>
2024-08-22 01:01:21 +0000

    "Adicionando arquivos da pasta pratica02

---
+++ praticas/pratica02/imprima_boletim.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  printf("Matricula\tA1\tA2\tMedia\n");
+  printf("20210010\t8.5\t5.0\t6.4\n");
+   printf("20215053\t4.7\t10.0\t7.9\n");
+   printf("20220027\t5.6\t8.3\t7.2\n");
+   printf("20220103\t9.2\t1.5\t4.6\n");
+  
+}
---
+++ praticas/pratica02/imprima_seu_nome.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  
+  printf("\njaneson pereira de alemida\n");
+  
+  return 0;
+}
