31f2c3fd71f2d468ce3e8c4c77676ac519eda2ef
janesonpa <pjaneson@gmail.com>
2024-11-17 15:15:31 -0300

    trabalho finalizado

--- trabalho/jogo_da_velha.c
+++ trabalho/jogo_da_velha.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 
 int main() {
     int opcao = 0;
@@ -13,7 +16,7 @@ int main() {
         printf("MENU PRINCIPAL\n\n");
         printf("1 - Iniciar jogo\n");
         printf("2 - Nomes dos jogadores\n");
-        printf("3 - Recordes\n");
+        printf("3 - Regras do jogo\n");
         printf("4 - sair\n");
         printf("Entre com uma opcao:");
         scanf("%i", &opcao);
@@ -29,7 +32,7 @@ int main() {
                         {'-', '|', '-', '|', '-'},
                         {' ', '|', ' ', '|', ' '},
                     };
-                    char move[3];
+                    char move[4];
                     int dest_x, dest_y;
                     int turno = 1;
                     char quem;
@@ -64,10 +67,10 @@ int main() {
                         int set = 1;
                         while (set == 1 && fora == 1) {
                             printf("Digite seu movimento (ex: a3) ou 'q' para sair: ");
-                            scanf("%s", move);
+                            scanf("%2s", move);
                             while (getchar() != '\n');
 
-                            if (move[0] == 'q') {
+                            if (move[0] == 'q' && move[1]=='\0') {
                                 fora =2;
                                 break;
                             }
@@ -109,7 +112,24 @@ int main() {
                                     (tabuleiro[0][0] == quem && tabuleiro[0][2] == quem && tabuleiro[0][4] == quem) ||  // Linha 3
                                     (tabuleiro[2][0] == quem && tabuleiro[2][2] == quem && tabuleiro[2][4] == quem) ||  // Linha 2
                                     (tabuleiro[4][0] == quem && tabuleiro[4][2] == quem && tabuleiro[4][4] == quem)) {  // Linha 1
-
+                                    system("clear");
+                                    printf("  a   b   c \n");
+                                    for (int i = 0; i < 5; i++) {
+                                        if ((5 - i) % 2 != 0) {
+                                            printf("%d ", (5 - i) / 2 + 1);
+                                        } else {
+                                            printf("  ");
+                                        }
+                                        for (int j = 0; j < 5; j++) {
+                                            printf("%c ", tabuleiro[i][j]);
+                                        }
+                                        if ((5 - i) % 2 != 0) {
+                                            printf("%d\n", (5 - i) / 2 + 1);
+                                        } else {
+                                            printf("\n");
+                                        }
+                                    }
+                                    printf("  a   b   c \n");
                                     printf("TEMOS UM VENCEDOR!\n");
                                     printf("O jogador %s venceu o jogo!\n", turno == 1 ? jogador1 : jogador2);
                                     fora = 0;
@@ -136,19 +156,69 @@ int main() {
                 }
                 break;
             case 2 :
-            printf("Nome dos jogadores\n");
-            printf("Digite o nome do jogador 1");
-            printf("Digite o nome do jogador 2");
+                system("clear");
+                printf("===============================\n\n");
+                printf("* * * NOME DOS JOGADORES * * *\n\n");
+                printf("===============================\n\n");
+                for(int i=1; i!=0; i++){
+                    char teste1[20]={0};
+                    char teste2[20]={0};
+        
+                    printf("Digite o nome do jogador 1:\n->");
+                    scanf("%s", jogador1);
+                    while (getchar() != '\n');
+                    for(int j=0; j<strlen(jogador1); j++){
+                        teste1[j] = toupper(jogador1[j]);
+                    }
+                    for(int l=0; l<strlen(jogador2); l++){
+                        teste2[l] = toupper(jogador2[l]);
+                    }
+                    if(strcmp(teste1, teste2)== 0){
+                        printf("O nome %s e igual a do jogador 2! Escolha outro nome...\n", jogador1);
+                        memset(jogador1, 0, sizeof(jogador1));
+                        getchar();
+                    }else{
+                        for(int k=1; k!=0; k++){
+                            printf("\n\nDigite o nome do jogador 2:\n->");
+                            scanf("%s", jogador2);
+                            while (getchar() != '\n');
+                            for(int l=0; l<strlen(jogador2); l++){
+                                teste2[l] = toupper(jogador2[l]);
+                            }
+                            if(strcmp(teste2, teste1)== 0){
+                                printf("O nome %s e igual a do jogador 2! Escolha outro nome...\n", jogador2);
+                                memset(jogador2, 0, sizeof(jogador2));
+                                getchar();
+                            }else{
+                                i=-1;
+                                break;
+                            }
+                        }
+                    }
+                }
+
                 break;
             case 3 :
-            printf("SEUS RECORDES\n");
+                system("clear");
+                printf("===============================\n\n");
+                printf("* * * REGRAS DO JOGO* * * \n");
+                printf("===============================\n\n");
+                printf("1.Comeco do jogo: O jogador que usa o X comeca.\n\n");
+                printf("2.Turnos alternados: Os jogadores se alternam colocando seus simbolos em uma das 9 celulas do tabuleiro. O jogador nao pode escolher uma celula ja ocupada.\n\n");
+                printf("3.Ganha quem formar uma linha: O jogo termina quando um jogador consegue formar uma linha (horizontal, vertical ou diagonal) de 3 simbolos iguais.\n\n");
+                printf("4.Empate: Se todas as 9 celulas forem preenchidas e ninguem formar uma linha, o jogo termina empatado.\n\n");
+                
+                printf("Pressione Enter para voltar ao menu...\n");
+                getchar();
+
                 break;
+            
             case 4 :
             printf("Ate mais");
                 break;
             default: ("Opcao invalida. Tente novamente!");
         }
-
+        
     }
     
     return 0;
